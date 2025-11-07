140c7cfb7f73311aeee15acc3977e722f63cef6e
KingTigh <20tpg1@queensu.ca>
2025-11-06 22:11:17 -0500

    fix ssr client issues

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,6 @@
 "use client";
-
-import '../styles/global.css';
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useState } from "react";
+import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }) {
-  // Create Query Client on the client only
-  const [queryClient] = useState(() => new QueryClient());
-
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
-  );
+  return <Component {...pageProps} />;
 }
